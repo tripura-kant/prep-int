3eabce5e906b69ddad0d0d87faa7249c4e3066ea
Tripura kant <tripura.kant@zoom.us>
2025-12-19 23:27:42 +0530

    new

---
+++ coding-practical/Q1.py
@@ -0,0 +1,19 @@
+'''
+Q1. Find Top 10 IPs by Frequency
+Log Type: Nginx / Apache access log
+ 192.168.1.10 - - [24/May/2025:10:21:05 +0000] "GET /index.html HTTP/1.1" 200 1024
+ Task: Extract the top 10 IP addresses by number of requests.
+'''
+
+
+ip_count = {}
+
+with open("access.log") as file:
+    for line in file:
+        ip = line.split()[0]
+        ip_count[ip] = ip_count.get(ip, 0) + 1
+
+sorted_ips = sorted(ip_count.items(), key=lambda x: x[1], reverse=True)
+
+for ip, count in sorted_ips[:10]:
+    print(f"{ip}: {count}")
---
+++ coding-practical/Q2.py
@@ -0,0 +1,11 @@
+# Q2. Filter Requests with 5xx Status Codes
+# Task: Extract all lines where the HTTP status code is in the 5xx range.
+
+log_file="access.log"
+
+
+with open(log_file) as f:
+    for line in f:
+        status = int(line.split()[-2])
+        if status >= 500:
+            print(line)
