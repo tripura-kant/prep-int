a67ac22e8e695773db396a5452545ed79d72db83
Tripura kant <tripura.kant@zoom.us>
2025-12-20 14:56:04 +0530

    dsad

--- coding-practical/Q5.py
+++ coding-practical/Q5.py
@@ -1,2 +1,13 @@
 # Q5. Extract Timestamp and Group Requests Per Minute
 # Task: Extract timestamps and count number of requests per minute.
+
+with open("access.log") as f:
+    for line in f:
+        parts = line.split()
+        if len(parts) < 9:
+            continue
+
+        status = parts[8]
+
+        if status.startswith("5"):
+            print(line.strip())
