084d3d3ceb7df2bede4783d3310a8c6705a96d84
Tripura kant <tripura.kant@zoom.us>
2025-12-23 19:56:44 +0530

    dsad

--- coding-practical/easy.py
+++ coding-practical/easy.py
@@ -5,7 +5,7 @@ with open(LOG_FILE) as f:
     for line in f:
         if "ERROR" in line:
             print(line)
-'''
+
 
 #Count total number of lines in a file
 LOG_FILE= "/Users/tripurakant/Documents/code/My_git/prep-int/coding-practical/app.log"
@@ -13,4 +13,15 @@ count = 0
 with open(LOG_FILE) as f:
     for line in f:
         count += 1
-    print(count)    
+    print("Total lines:",count)    
+
+'''
+
+# Count total number of words in a file
+LOG_FILE= "/Users/tripurakant/Documents/code/My_git/prep-int/coding-practical/app.log"
+count = 0
+with open(LOG_FILE) as file:
+    for line in file:
+        words = line.split()
+        count = len(words)
+print("Total words:", count)
