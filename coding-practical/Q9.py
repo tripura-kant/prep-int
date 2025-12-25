078d692e6f84cedf0ca39c1822af06cdfe770aac
Tripura kant <tripura.kant@zoom.us>
2025-12-25 19:56:09 +0530

    Thu Dec 25 19:56:09 IST 2025

---
+++ coding-practical/Q9.py
@@ -0,0 +1,7 @@
+'''
+Q9. Extract Top 10 Referrers
+Log:
+ "GET /home HTTP/1.1" 200 1024 "https://google.com/search?q=..." "Mozilla/5.0"
+ Task: Extract the referrer field and show the top 10 sources.
+'''
+
