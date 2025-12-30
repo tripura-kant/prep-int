c1d60b6c778aa7660c753ba2658639853a7503dc
Tripura kant <tripura.kant@zoom.us>
2025-12-30 17:56:28 +0530

    Tue Dec 30 17:56:28 IST 2025

--- coding-practical/Q10.py
+++ coding-practical/Q10.py
@@ -1,11 +1,15 @@
 # Q10. Detect Possible Brute Force IPs
 # Task: Find IPs that accessed the /login endpoint more than 100 times in a day.
 
-log_file = "/Users/tripurakant/Documents/code/My_git/prep-int/coding-practical/ref.log"
+log_file = "/Users/tripurakant/Documents/code/My_git/prep-int/coding-practical/access.log"
 
 ref_count = {}
 
 with open(log_file) as f:
     for line in f:
         parts = line.split()
-        ip = parts[9]
+        ip = parts[0]
+        # print(ip)
+        endpoint = parts[6]
+        if endpoint == "/login":
+            print(f'ip is {ip} : endpoint is {endpoint}')
